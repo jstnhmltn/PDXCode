845462851cdcdfd8325eeadde9b6150a72c5fa40
Justin Hamilton <justinhamilton@Justins-MacBook-Pro.local>
2020-04-28 20:08:32 -0700

    2020-04-28

--- 3 Django/Lab1/djangogirls/blog/urls.py
+++ 3 Django/Lab1/djangogirls/blog/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-# still working
-# getting an error after manage.py runserver
-# because the view is created yet
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
