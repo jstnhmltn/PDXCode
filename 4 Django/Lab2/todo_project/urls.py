e2b82f85e610861e877a0e666fd1e1b2b674122e
Justin Hamilton <justinhamilton@Justins-MacBook-Pro.local>
2020-05-04 19:33:58 -0700

    2020-05-04

--- 4 Django/Lab2/todo_project/urls.py
+++ 4 Django/Lab2/todo_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo_app/', include('todo_project.urls')),
+    path('', include('todo_app.urls')),
 ]
