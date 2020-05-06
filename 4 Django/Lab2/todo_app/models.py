45e0faf61b1a24166e47dbda2b7146674937a21f
Justin Hamilton <justinhamilton@Justins-MacBook-Pro.local>
2020-05-05 20:40:26 -0700

    2020-05-05

--- 4 Django/Lab2/todo_app/models.py
+++ 4 Django/Lab2/todo_app/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Todo(models.Model):
     added_date = models.DateTimeField()
     text = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
+
--- 4 Django/Lab2/todo_app/views.py
+++ 4 Django/Lab2/todo_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
-from todo_app.models import Todo
+from .models import Todo
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Todo.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'main/index.html', context=context)
