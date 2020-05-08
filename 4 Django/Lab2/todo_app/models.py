de22390ad3f0bbd3e45ac3cf632b876a1fefac43
Justin Hamilton <justinhamilton@Justins-MacBook-Pro.local>
2020-05-07 19:22:21 -0700

    2020-05-07

--- 4 Django/Lab2/todo_app/models.py
+++ 4 Django/Lab2/todo_app/models.py
@@ -5,3 +5,5 @@ class Todo(models.Model):
     text = models.CharField(max_length=200)
     is_completed = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.text
--- 4 Django/Lab2/todo_app/urls.py
+++ 4 Django/Lab2/todo_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'todo_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.addTodo, name='add'),
+    path('complete/<todo_id>', views.completeTodo, name='complete'),
+    path('deletecomplete', views.deleteCompleted, name='deletecomplete'),
+    path('deleteall', views.deleteAll, name='deleteall')
 ]
