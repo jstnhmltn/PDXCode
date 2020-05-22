0ee666373a1a985315fd143ff231bca90396e7c7
Justin Hamilton <justinhamilton@Justins-MacBook-Pro.local>
2020-05-21 18:51:23 -0700

    needs html and views

--- 4_Django/Lab4/library_app/models.py
+++ 4_Django/Lab4/library_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    published_date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
