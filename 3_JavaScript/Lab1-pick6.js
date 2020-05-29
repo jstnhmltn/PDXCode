a359f86a675036bc2892b631967fbf0edf5dbd04
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-05-28 21:02:00 -0700

    2020-05-28

--- 3_JavaScript/Lab1-pick6.js
+++ 3_JavaScript/Lab1-pick6.js
@@ -1,51 +1,30 @@
-let ticket = [];
-let winner = [];
-let match = 0;
-
-function win_pick6() {
-    for (i=0; i<6; i++) {
-        winner.push(Math.floor(Math.random() * 100));
-        }   
-    console.log("win  ticket", winner);
+function pick6(){
+    for (i=0; i<6; i++){
+        return Math.floor(Math.random() * 100);
+    }
 }
-win_pick6();
 
-function tix_pick6() {
-    for (i=0; i<6; i++) {
-        ticket.push(Math.floor(Math.random() * 100));
-        }   
-    console.log("user ticket", ticket);
-}
-tix_pick6();
-
-function matches() {
-    let payout = [0, 4, 7, 100, 50000, 1000000, 25000000];
-    for (i=0; i<6; ++i) {
-        if (ticket[i] === winner[i]) {
+function matches(ticket, winner){
+    let match = 0;
+    for (i=0; i<6; i++){
+        if (ticket[i] === winner[i]){
             match++;
         }
     }
-    console.log("number of matches", match);
+    return match;
 }
-matches();
 
-/*
-function play100k() {
+function main(){
+    let expense = 0;
+    let payout = [0, 4, 7, 100, 50000, 1000000, 25000000];
     winner = pick6();
-    bal = 0;
-    for (let i=0; i<100000; ++i) {
-        ticket = pick6();
-        bal -= 2;
-        payout = matches(winner, ticket);
-        bal +- payout;
+    for (i=0; i<10; i++){
+        expense += 2;
+        match = matches(winner, pick6());  
+        balance = payout[match];
     }
-    return console.log(bal);
-}
-
-function main() {
-    for (i=0; i<100; ++i) {
-        play100k();
-    }
+    console.log("win  ticket", winner);
+    console.log("user ticket", ticket);
+    console.log("payout", payout[match]);
 }
 main();
-*/
