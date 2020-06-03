a359d12515fdf8d464a73a6a7de6c5e5735752cc
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-02 21:15:45 -0700

    2020-06-02

--- 3_JavaScript/Lab2-Todo_List/main.js
+++ 3_JavaScript/Lab2-Todo_List/main.js
@@ -1,1 +1,28 @@
-document.getElementById
+add_button.onclick = function() {
+    let todo_input = document.querySelector('#todo_input');
+    let list = document.querySelector('#todo_list');
+    let completed_list = document.querySelector('#completed_list');
+    let text = todo_input.value;
+    if (text === '') {
+        return;
+    }
+
+    todo_input.value = '';
+    let li = document.createElement('li');
+    li.classList.add('todo_item');
+    let text_div = document.createElement('div');
+    text_div.innerHTML = text;
+
+    let button_complete = document.createElement('button');
+    button_complete.innerHTML = 'COMPLETE';
+    button_complete.onclick = function() {
+        list.removeChild(this.parentElement);
+        let li = document.createElement('li');
+        li.innerText = text;
+        completed_list.appendChild(li);
+    };
+
+    li.appendChild(text_div);
+    li.appendChild(button_complete);
+    list.appendChild(li);
+};
