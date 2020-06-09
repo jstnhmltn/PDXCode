fb04517e7b6747e0416c98c82418bc0cd093125f
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-08 21:14:15 -0700

    2020-06-08

---
+++ 5_Capstone/yearbook/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    firt_name = models.CharField(max_length=75)
+    last_name = models.Charfield(max_length=75)
+    
---
+++ 5_Capstone/yearbook/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth import HTTPRespnse
+
+# Create your views here.
+def home(render):
+    return 
