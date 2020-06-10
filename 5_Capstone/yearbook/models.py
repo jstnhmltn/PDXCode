b8c06c282dcc24241ea49e91900bdb714390ba62
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-09 21:01:00 -0700

    add user_image to admin panel

--- 5_Capstone/yearbook/models.py
+++ 5_Capstone/yearbook/models.py
@@ -4,13 +4,12 @@ from django.contrib.auth import get_user_model
 
 class Upload(models.Model):
     title = models.CharField(max_length = 200)
-    text = models.TextField(max_length = 500)
-    status = models.BooleanField(default = False)
-    created_at = models.DateTimeField(auto_now_add = True)
-    completed_at = models.DateTimeField(blank = True, null = True)
-    task_duration = models.DecimalField(max_digits = 4, decimal_places = 2, blank = True, null = True)
+    description = models.TextField(max_length = 500)
+    #upload_date = models.DateTimeField(auto_now_add = True)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    user_image = models.ImageField(null=True)
 
     def __str__(self):
         return self.title
+
     
