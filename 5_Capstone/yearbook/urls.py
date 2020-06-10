8e116999fe3c3ff10d1e7c71bbeffc4fb436b224
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-09 20:16:51 -0700

    2020-06-09

--- 5_Capstone/yearbook/urls.py
+++ 5_Capstone/yearbook/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/pages/', view.about, name='about'),
-    path('/pages/', views.contact, name='contact)',
-    path('/pages/', view.news, name='news'),
+    path('pages/', views.about, name='about'),
+    path('pages/', views.contact, name='contact'),
+    path('pages/', views.news, name='news'),
 ]
--- 5_Capstone/yearbook/views.py
+++ 5_Capstone/yearbook/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth import HTTPRespnse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import Upload
 
 # Create your views here.
-def home(render):
-    return HTTPResponse
+def home(request):
+    return HttpResponse
+
+def about(request):
+    pass
+
+def contact(request):
+    pass
+
+def news(request):
+    pass
