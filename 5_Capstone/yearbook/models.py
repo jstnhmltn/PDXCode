2335c321ad90680d200c91a40d43b34ff6bf8ceb
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-09 19:28:04 -0700

    added templates

--- 5_Capstone/yearbook/models.py
+++ 5_Capstone/yearbook/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class User(models.Model):
     firt_name = models.CharField(max_length=75)
     last_name = models.Charfield(max_length=75)
+
+    def __str__(self):
+        return
     
--- 5_Capstone/yearbook/views.py
+++ 5_Capstone/yearbook/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth import HTTPRespnse
 
+from .models import User
+
 # Create your views here.
 def home(render):
     return HTTPResponse
