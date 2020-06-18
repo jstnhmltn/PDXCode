cd5bd123ac458c941738dff343225c85daa1bfe7
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-18 14:46:55 -0700

    update showcase db config

--- 5_Capstone/showcase/migrations/0002_auto_20200609_2059.py
+++ 5_Capstone/showcase/migrations/0002_auto_20200609_2059.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('yearbook', '0001_initial'),
+        ('showcase', '0001_initial'),
     ]
 
     operations = [
