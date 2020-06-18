c92b236a8b931646d97af4a8aceeea1308c0fa7f
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-18 14:39:45 -0700

    rename yearbook app to showcase

--- 5_Capstone/mysite/settings.py
+++ 5_Capstone/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'yearbook',
+    'showcase',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
