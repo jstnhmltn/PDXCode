7a3449f075125791c84a1a746eff1c638c49358b
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-18 16:09:37 -0700

    update showcase.models.py

--- 5_Capstone/showcase/models.py
+++ 5_Capstone/showcase/models.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from django.db import models
-from django.contrib.auth import get_user_model
 
 class Upload(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField(max_length = 500)
-    #upload_date = models.DateTimeField(auto_now_add = True)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
-    user_image = models.ImageField(null=True)
+    upload_date = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(null=True)
 
     def __str__(self):
         return self.title
