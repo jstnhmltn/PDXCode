5ff0d100bcb045517715d5af75b21f25d69ca492
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-18 02:23:50 -0700

    restructure

--- 5_Capstone/yearbook/urls.py
+++ 5_Capstone/yearbook/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('pages/', views.contact, name='contact'),
-    path('pages/', views.register, name='register'),
+  
 ]
