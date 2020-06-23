8b0edcaeeb4cd23aca7ca9c3f8f680194803e4a4
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-22 21:40:31 -0700

    added image functionality

--- 5_Capstone/mysite/urls.py
+++ 5_Capstone/mysite/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', include('showcase.urls')),
     path('', include('journal.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 5_Capstone/showcase/models.py
+++ 5_Capstone/showcase/models.py
@@ -5,7 +5,7 @@ class Upload(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField(max_length = 100)
     upload_date = models.DateTimeField(auto_now_add = True)
-    image = models.ImageField(null=True, upload_to='media/img')
+    image = models.ImageField(null=True, upload_to='img/', verbose_name='title')
 
     def __str__(self):
         return self.title
