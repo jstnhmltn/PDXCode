83e921a15a80b989eebd51e2a4a01467effc5c3f
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-23 02:48:05 -0700

    update journal/views.py

--- 5_Capstone/journal/models.py
+++ 5_Capstone/journal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
@@ -11,4 +12,4 @@ class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    # post = models.ForeignKey("Post", on_delete=models.CASCADE)
