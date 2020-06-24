22eb19679488f418c21d86572f38166de6cf67fc
Justin Hamilton <61444651+jstnhmltn@users.noreply.github.com>
2020-06-24 13:38:26 -0700

    not working: image upload from html

--- 5_Capstone/showcase/views.py
+++ 5_Capstone/showcase/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Upload
@@ -13,8 +13,34 @@ def showcase_detail(request, pk):
     context = {'upload': upload}
     return render(request, 'showcase_detail.html', context)
 
-# @ login_required
-# def showcase_post(request, pk):
-#     upload = Upload.objects.new(pk=pk)
-#     context = {'upload': upload}
-#     return render(request, 'showcase_post.html,')
+@ login_required
+def showcase_post(request):
+    if request.method == 'GET':
+        return render(request, 'showcase_post.html')
+    elif request.method == 'POST':
+        upload = Upload.objects.create()
+        context = {'upload': upload}
+        return render(request, 'showcase_index.html', context)
+
+
+
+
+
+#     @login_required
+# def add_todo(request):
+#     if request.method == 'GET':
+#         return render(request, 'todos/add.html')
+#     elif request.method == 'POST':
+#         title = request.POST['title']
+#         text = request.POST['text']
+#         if (request.POST['status'] == 'False'):
+#             status = False
+#         else:
+#             status = True
+#         todo = Todo.objects.create(
+#             title = title, 
+#             text = text, 
+#             status = status,
+#             user = request.user
+#         )
+#         return redirect('list')
